31516164c973c57f9402c6d6d35508cf0bfbdb06
Regis Camimura <regiscamimura@gmail.com>
2017-01-24 14:16:20 -0200

    added hours and bold numbers

--- sane-sart-counter.php
+++ sane-sart-counter.php
@@ -10,7 +10,7 @@ $text_color = imagecolorallocate($img, 236, 236, 236);
 $font = 'theme/micross.ttf';
 $fontB = 'theme/microssB.ttf';
 
-$date = strtoupper(date('l, F j, Y'));
+$date = strtoupper(date('l, F j, Y - G:i:s'));
 $footer = "SEXUAL ASSAULTS SINCE ".date("1.01.Y");
 $rate = '1.85';
 
@@ -25,14 +25,14 @@ $numbers = str_split($count);
 $number_box_width = ($width - $margin) / 6;
 
 
-imagettftext($img, 8, 0, 65, 20, $text_color, $font, $date);
+imagettftext($img, 8, 0, 32, 20, $text_color, $font, $date);
 imagettftext($img, 10, 0, 110, 45, $text_color, $fontB, $rate);
 
 foreach ($numbers as $k=>$number) {
-	$box = imagettfbbox( 35 , 0 , $font , $number );
+	$box = imagettfbbox( 35 , 0 , $fontB , $number );
 		
 	$x = floor(($number_box_width - $box[2]) / 2);
-	imagettftext($img, 35, 0, ($x + ($k * $number_box_width)) + 5, 135, $text_color, $font, $number);
+	imagettftext($img, 35, 0, ($x + ($k * $number_box_width)) + 5, 135, $text_color, $fontB, $number);
 }
 
 imagettftext($img, 10, 0, 22, 178, $text_color, $font, $footer);
