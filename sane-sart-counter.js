8eda3f304b5bf89ab9ac6a4db483a0528e31f98d
Regis Camimura <regiscamimura@gmail.com>
2017-02-06 16:01:30 -0200

    meta-reading of script src

--- sane-sart-counter.js
+++ sane-sart-counter.js
@@ -1,7 +1,12 @@
 var WIDGET = (function(WIDGET) {
 
 	WIDGET.rate = '1.85';
-	if (WIDGET.base_url === undefined) WIDGET.base_url = 'https://cdn.rawgit.com/dleetinc/sanesartcounter/bde42060/';
+
+	var scripts = document.getElementsByTagName('script');
+	var index = scripts.length - 1;
+	var myScript = scripts[index];
+
+	if (WIDGET.base_url === undefined) WIDGET.base_url = myScript.src.replace('sane-sart-count.js');//https://cdn.rawgit.com/dleetinc/sanesartcounter/bde42060/';
 	WIDGET.link = 'http://www.sane-sart.com/';
 	
 	var base_url = WIDGET.base_url;
