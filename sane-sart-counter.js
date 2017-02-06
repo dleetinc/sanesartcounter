4ff2c3974d9a98c827c2abdd87c585cd5ff75cfc
Regis Camimura <regiscamimura@gmail.com>
2017-02-06 15:18:38 -0200

    rawgit cdn url

--- sane-sart-counter.js
+++ sane-sart-counter.js
@@ -1,7 +1,7 @@
 var WIDGET = (function(WIDGET) {
 
 	WIDGET.rate = '1.85';
-	if (WIDGET.base_url === undefined) WIDGET.base_url = 'https://cdn.rawgit.com/dleetinc/sanesartcounter/0732c33f/';
+	if (WIDGET.base_url === undefined) WIDGET.base_url = 'https://cdn.rawgit.com/dleetinc/sanesartcounter/1aea142a/';
 	WIDGET.link = 'http://www.sane-sart.com/';
 	
 	var base_url = WIDGET.base_url;
